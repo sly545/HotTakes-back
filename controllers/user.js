6665b2e8a44c628f069027540e1a0d95f2a34573
sly545 <sylvainferrandes@gmail.com>
2023-04-26 15:04:04 +0200

    changement multer config

--- controllers/user.js
+++ controllers/user.js
@@ -1,50 +1,52 @@
 const bcrypt = require("bcrypt");
-
 const jwt = require('jsonwebtoken');
+const User = require("../models/user");
 
-const User = require("../models/user")
-
-//logique pour que l'utilisateur crée le compte
- 
+// Fonction pour gérer l'inscription d'un utilisateur
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-          email: req.body.email,
-          password: hash
-        });
-        user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
-  };
+  // On utilise bcrypt pour hasher le mot de passe de l'utilisateur
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      // On crée un nouvel utilisateur avec l'email et le mot de passe hashé
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      // On enregistre l'utilisateur dans la base de données
+      user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
 
-
-
- //logique pour que l'utilisateur pour le login
-
-  exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
+// Fonction pour gérer la connexion d'un utilisateur
+exports.login = (req, res, next) => {
+  // On cherche l'utilisateur dans la base de données en utilisant son email
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      // Si l'utilisateur n'est pas trouvé, on renvoie une erreur
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+      }
+      // On utilise bcrypt pour comparer le mot de passe entré par l'utilisateur avec le mot de passe hashé stocké dans la base de données
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          // Si la comparaison échoue, on renvoie une erreur
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          // Si la comparaison réussit, on renvoie un token JWT signé contenant l'ID de l'utilisateur et une durée de validité de 24 heures
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' }
+            )
+          });
         })
         .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
--- middlewares/multer_conf.js
+++ middlewares/multer_conf.js
@@ -1,7 +1,6 @@
-
-
 const multer = require("multer");
-//types de fichier qui sont axepter
+
+// Liste des types MIME acceptés pour les fichiers image
 const MIME_TYPES = {
   "image/jpg": "jpg",
   "image/jpeg": "jpg",
@@ -15,43 +14,37 @@ const MIME_TYPES = {
   "image/webp": "webp",
 };
 
-const storage = multer.diskStorage({
+// Fonction pour vérifier si le type MIME d'un fichier est accepté
+const isAcceptedMimeType = (mimetype) => {
+  return Object.keys(MIME_TYPES).includes(mimetype);
+};
 
+// Fonction pour générer le nom du fichier en utilisant son nom d'origine, la date actuelle et son extension
+const generateFileName = (file) => {
+  const name = file.originalname.split(" ").join("_");
+  const extension = MIME_TYPES[file.mimetype];
+  return name + Date.now() + "." + extension;
+};
+
+// Configuration de l'objet de stockage pour multer
+const storage = multer.diskStorage({
+  // Fonction pour déterminer le dossier de destination des fichiers téléchargés
   destination: (req, file, callback) => {
-    callback(null, "images");
+    // Si le type MIME du fichier est accepté, il est stocké dans le dossier 'images', sinon dans le dossier 'isole'
+    callback(null, isAcceptedMimeType(file.mimetype) ? "images" : "isole");
   },
-  
+  // Fonction pour déterminer le nom du fichier téléchargé
   filename: (req, file, callback) => {
-    
-    const name = file.originalname.split(" ").join("_");
-    
-    const extension = MIME_TYPES[file.mimetype];
-    
-    if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/png" ||
-      file.mimetype === "image/jpg" ||
-      file.mimetype === "image/bmp" ||
-      file.mimetype === "image/gif" ||
-      file.mimetype === "image/ico" ||
-      file.mimetype === "image/svg" ||
-      file.mimetype === "image/tiff" ||
-      file.mimetype === "image/tif" ||
-      file.mimetype === "image/webp"
-    ) {
-      
-      callback(null, name + Date.now() + "." + extension);
-      // si ce n'est pas un fichier image
+    if (isAcceptedMimeType(file.mimetype)) {
+      // Si le type MIME du fichier est accepté, on utilise la fonction generateFileName pour générer son nom
+      callback(null, generateFileName(file));
     } else {
-      
+      // Si le type MIME du fichier n'est pas accepté, on affiche un message d'erreur et on ajoute l'ID de l'utilisateur au nom du fichier
       console.log("fichier non accepté seul les images sont autoriser");
-      // cette fonction callback va isoler le fichier qui n'est pas une image.
-      callback( 
-        null,
-        "isole/" + req.auth.userId + "_" + name + Date.now() + "." + extension
-      );
+      callback(null, req.auth.userId + "_" + generateFileName(file));
     }
   },
 });
 
+// Export du middleware multer configuré avec l'objet de stockage créé précédemment
 module.exports = multer({ storage }).single("image");
